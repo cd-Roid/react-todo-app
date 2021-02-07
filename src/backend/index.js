d7c84d966697e99f125afb8e457087eaabd2225e
cd-Roid <giyawe98@gmail.com>
2021-02-07 23:41:18 +0100

    created CRUD methods

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const  _ = require('express-json');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
+const { ObjectId } = require('mongodb');
+
+
 const app = express();
+const jsonParser = bodyParser.json();
 const PORT = 3001;
 const uri = `mongodb+srv://cd-RoiD:${process.env.MONGO_PASSWORD}@cluster0.cpt7m.mongodb.net/Todos?retryWrites=true&w=majority`;
 
@@ -11,11 +16,54 @@ const uri = `mongodb+srv://cd-RoiD:${process.env.MONGO_PASSWORD}@cluster0.cpt7m.
   if (err) {
     throw err;
   }
- // const cursor = await client.db('Todos').collection('todos').insertOne({text:"Run mongodb",done: false})
 
+  db = client.db('Todos');
 });
 
+app.get('/', async (req , res)=>{
+    try {
+        const todos = await db.collection('todos').find({}).toArray();
+        res.json(todos); 
+    } catch (error) {
+        throw error;
+    }
+})
+
+
+app.post('/', jsonParser,async (req , res)=>{
+    try {
+        const data = {text: req.body.text, done: false}
+        await db.collection('todos').insertOne(data);
+        res.json({desc: "Successfully added"});
+    } catch (error) {
+        throw error;
+    }
+})  
+
+
+app.put('/:id', async (req , res)=>{
+    try {
+
+        await db.collection('todos').findOneAndUpdate({_id: ObjectId(req.params.id)},{$set:{'done':false}});
+        res.json({desc: "Successfully updated"});
+    } catch (error) {
+        throw error;
+    }
+})
+
+app.delete('/:id', async (req , res)=>{
+    try {
+
+        await db.collection('todos').deleteOne({_id: ObjectId(req.params.id)});
+        res.json({desc: "Successfully deleted"});
+    } catch (error) {
+        throw error;
+    }
+})
+
 app.use(_);
+app.use(jsonParser);
 app.listen(PORT, () => {
   console.log(`Todo Backend listening at http://localhost:${PORT}`);
 });
+
