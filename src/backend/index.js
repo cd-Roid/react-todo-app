8faec756e0ebdcd6b0161a88eb12b5fb69e5a704
cd-Roid <giyawe98@gmail.com>
2021-02-07 22:02:42 +0100

    Addded boilerplate for Mongo connection

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const  _ = require('express-json');
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
-const port = 3001;
+const PORT = 3001;
+const uri = `mongodb+srv://cd-RoiD:${process.env.MONGO_PASSWORD}@cluster0.cpt7m.mongodb.net/Todos?retryWrites=true&w=majority`;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+
+ MongoClient.connect(uri,{ useUnifiedTopology: true }, async function(err, client) {   
+  if (err) {
+    throw err;
+  }
+ // const cursor = await client.db('Todos').collection('todos').insertOne({text:"Run mongodb",done: false})
+
+});
 
 app.use(_);
-app.listen(port, () => {
-  console.log(`Todo Backend listening at http://localhost:${port}`);
-})
+app.listen(PORT, () => {
+  console.log(`Todo Backend listening at http://localhost:${PORT}`);
+});
