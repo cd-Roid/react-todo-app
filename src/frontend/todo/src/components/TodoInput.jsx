4644deaf4700ff1e34696ca9c70c20cad99b1e7c
cd-Roid <giyawe98@gmail.com>
2021-02-08 16:57:24 +0100

    function to submit new Todo

--- src/frontend/todo/src/components/TodoInput.jsx
+++ src/frontend/todo/src/components/TodoInput.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import {useState} from 'react';
+
+
 
 function TodoInput() {
+
+    const [newTodo, setNewTodo] = useState('');
+
+    const createTodo = async (e) => {
+        e.preventDefault();
+        const data = {"text": newTodo, "done": false}
+        await fetch('http://localhost:3001',{
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+
+        })
+    } 
+
+    
     return (
         <div>
             <form  method="post">
-             <input type="text" name="" id=""/>
-             <button type="submit">Add</button>
+             <input type="text"  value={newTodo} onChange= { (e) => setNewTodo(e.target.value)}></input>
+             <button onClick={createTodo}>Add</button>
             </form>
         </div>
     )
