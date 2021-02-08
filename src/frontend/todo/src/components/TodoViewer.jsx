7a2c7cd4896e554b5339bbbeb1604ba8c5d53aab
cd-Roid <giyawe98@gmail.com>
2021-02-08 17:17:56 +0100

    function for changing done status

--- src/frontend/todo/src/components/TodoViewer.jsx
+++ src/frontend/todo/src/components/TodoViewer.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-
+import {React, useState} from 'react';
 
 
 
 function TodoViewer(todo, deleteTodo) {
+
 const {text, done, _id} = todo.todo;
+const [doneStatus, setDone] = useState(done);    
+
+const setDoneStatus = async () => {
+    let data = {"done": doneStatus}
+    console.log(data);
+    await fetch(`http://localhost:3001/${_id}`, {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+
+    })
+    setDone(!done); 
+}
 
     return (
         <div>
             <div>{text}</div>
-            <input type="checkbox" checked={done}></input>
+            <input type="checkbox" checked={done} onClick={setDoneStatus}></input>
             <i onClick={()=>deleteTodo(_id)}>Delete</i>
         </div>
     )
