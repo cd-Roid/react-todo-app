7e74e9c94b8f4d5064fed1933523721d80509b25
cd-Roid <giyawe98@gmail.com>
2021-02-08 02:12:57 +0100

    added Delete Function

--- src/frontend/todo/src/components/TodoViewer.jsx
+++ src/frontend/todo/src/components/TodoViewer.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 
+async function deleteTodo(id) {
+    await fetch(`http://localhost:3001/${id}`, {
+        method: 'DELETE',
+        mode: 'cors',
+    });
 
+}
 
 
 function TodoViewer(todo) {
-const {text, done} = todo.todo;
+const {text, done, _id} = todo.todo;
 
     return (
         <div>
             <div>{text}</div>
             <input type="checkbox" checked={done}></input>
+            <i onClick={()=>deleteTodo(_id)}>Delete</i>
         </div>
     )
 };
