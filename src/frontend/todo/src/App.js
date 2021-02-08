78d5fb82db69b75905160e30b9dbb74f2b6c5313
cd-Roid <giyawe98@gmail.com>
2021-02-08 01:08:59 +0100

    fetching data from backend

--- src/frontend/todo/src/App.js
+++ src/frontend/todo/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 import TodoViewer from './components/TodoViewer';
+import {useState, useEffect} from 'react';
+
+
 function App() {
+
+  const [todo, setTodo] = useState([]);
+
+  const fetchTodos = async () =>{ 
+      const res = await fetch('http://localhost:3001');
+      const data = await res.json();
+      setTodo(data);
+  }
+
+  useEffect(()=>{
+      console.log("use Effect running");
+      fetchTodos();
+  },[])
+
+
   return (
     <div>
-      <TodoViewer />
+      {
+        todo.map(todo => <TodoViewer todo ={ todo }/>)
+      }
     </div>
   );
 }
